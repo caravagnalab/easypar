d4ef4f0562b52f8d438c69db3f9570425525277f
Salvatore Milite <militesalvatore@gmail.com>
2022-02-03 12:10:00 +0100

    slurm support

---
+++ R/default_SBATCH_config.R
@@ -0,0 +1,40 @@
+#' Deafult SLURM setting to submit array jobs to SLURM clusters.
+#' 
+#' @description Deafult SBATCH setting to submit array jobs to 
+#' LSF clusters. This is handy to obtain a default input, and
+#' implement custom modificiations. These parameters are to be
+#' used by function \code{run_slurm}.
+#' 
+#' @seealso run_lsf
+#'
+#' @return A named list of SBATCH settings.
+#' 
+#' @export
+#'
+#' @examples
+#' custom_SBATCH = default_SBATCH_config(W = '12:00')
+#' custom_SBATCH$`-nodes` = 10
+#' custom_SBATCH$`--partition` = "my_queue"
+
+default_SBATCH_config = function(
+  job = 'EASYPAR_Runner',
+  partition = 'cpu',
+  n = 1,
+  cpus = 16,
+  A = "",
+  time = '4:00',
+  o = 'log/output.%A-%a.log',
+  mem = "1040M"
+)
+{
+  list(
+    `--job-name` = job,
+    `--partition` = partition,
+    `--nodes` = n,
+    `--cpus-per-task` = cpus, 
+    `--array` = A,
+    `--time` = time,
+    `--output` = o,
+    `--mem-per-cpu` = mem
+  )
+}
