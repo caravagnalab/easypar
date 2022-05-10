6921ea2496c369f05e80ede87af54555acc2ec21
Giulio Caravagna <gcaravagn@gmail.com>
2022-05-10 13:00:39 +0200

    Test actions

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,8 @@
 # easypar 1.0.0
 
+* First official easypar release
+* Support for two distinct HPC job-submission systems
+
 # easypar 0.2.0
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/default_SBATCH_config.R
+++ R/default_SBATCH_config.R
@@ -1,18 +1,18 @@
 #' Deafult SLURM setting to submit array jobs to SLURM clusters.
-#' 
-#' @description Deafult SBATCH setting to submit array jobs to 
+#'
+#' @description Deafult SBATCH setting to submit array jobs to
 #' LSF clusters. This is handy to obtain a default input, and
 #' implement custom modificiations. These parameters are to be
 #' used by function \code{run_slurm}.
-#' 
+#'
 #' @seealso run_lsf
 #'
 #' @return A named list of SBATCH settings.
-#' 
+#'
 #' @export
 #'
 #' @examples
-#' custom_SBATCH = default_SBATCH_config(W = '12:00')
+#' custom_SBATCH = default_SBATCH_config(time = '12:00')
 #' custom_SBATCH$`-nodes` = 10
 #' custom_SBATCH$`--partition` = "my_queue"
 
@@ -31,7 +31,7 @@ default_SBATCH_config = function(
     `--job-name` = job,
     `--partition` = partition,
     `--nodes` = n,
-    `--cpus-per-task` = cpus, 
+    `--cpus-per-task` = cpus,
     `--array` = A,
     `--time` = time,
     `--output` = o,
